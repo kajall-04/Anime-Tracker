3b4f40843aee9a4677948b26df892470dc577434
kajal Pareek <kajalpareek1@gmail.com>
2025-04-21 12:45:35 +0530

    Initial commit

---
+++ nexode-dashboard/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+app.get('/user', (req, res) => {
+  res.json({
+    username: "@NexodeUser",
+    bio: "Loves Psychological Anime",
+    currentlyWatching: "Jujutsu Kaisen - Ep 20",
+    progress: 80,
+    watchToday: "2h 10m",
+    watchWeek: "10h 30m"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
